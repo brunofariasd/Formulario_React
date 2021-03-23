4125fc0cd3580f1edfadc36fdfa0b1829b8301b7
Bruno Farias <bruno_fariasd@hotmail.com>
2021-03-23 09:49:22 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import FormularioCadastro from './components/FormularioCadastro'
+import './App.css';
+
+function App() {
+  return (
+    <FormularioCadastro/>
+  );
+}
+
+export default App;
---
+++ src/components/FormularioCadastro.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function FormularioCadastro(){
+    return(
+        <h1>HELLO WORD</h1>
+    )
+}
+
+export default FormularioCadastro;
